633e546c892dac4f731bd8ac1629f887aff30124
mhtien <59407030+mhtien@users.noreply.github.com>
2020-04-13 13:47:23 +0100

    Image Carousel - separated key press function code from event click line

--- script.js
+++ script.js
@@ -39,55 +39,57 @@ const imageFive = document.getElementsByClassName("image-5")[0];
 const playButton = document.getElementsByClassName("play")[0];
 const pauseButton = document.getElementsByClassName("pause")[0];
 
-let slidesArray = [imageOne, imageTwo, imageThree, imageFour,imageFive];
+let slidesArray = [imageOne, imageTwo, imageThree, imageFour, imageFive];
 let carouselLength = slidesArray.length;
 let i = 0;
 
 function slideRight(event) {
-    
-        // moves active slide out of frame
-        slidesArray[i].classList.add("img-left");
-        slidesArray[i].classList.remove("img-active");
-    
-        // moves slide into frame
-        slidesArray[i+1].classList.add("img-active");
-        slidesArray[i+1].classList.remove("img-right");
 
-        // lines up next frames
-        slidesArray[i+2].classList.add("img-right");
-        slidesArray[i+2].classList.remove("img-hidden");
+    // moves active slide out of frame
+    slidesArray[i].classList.add("img-left");
+    slidesArray[i].classList.remove("img-active");
 
-        // for all other frames
-        slidesArray[i+3].classList.remove("img-left")
-        slidesArray[i+3].classList.add("img-hidden");
-        // cycling through frames
-        slidesArray.push(slidesArray.shift())
-    
+    // moves slide into frame
+    slidesArray[i + 1].classList.add("img-active");
+    slidesArray[i + 1].classList.remove("img-right");
+
+    // lines up next frames
+    slidesArray[i + 2].classList.add("img-right");
+    slidesArray[i + 2].classList.remove("img-hidden");
+
+    // for all other frames
+    slidesArray[i + 3].classList.remove("img-left")
+    slidesArray[i + 3].classList.add("img-hidden");
+ 
+    // restarts array order
+    slidesArray.push(slidesArray.shift())
+
 }
 
 function slideLeft(event) {
 
-        // moves active slide out of frame
-        slidesArray[i].classList.add("img-right");
-        slidesArray[i].classList.remove("img-active");
+    // moves active slide out of frame
+    slidesArray[i].classList.add("img-right");
+    slidesArray[i].classList.remove("img-active");
+
+    // moves slide into frame
+    slidesArray[carouselLength - 1].classList.add("img-active");
+    slidesArray[carouselLength - 1].classList.remove("img-left");
+
+    // lines up next frames
+    slidesArray[carouselLength - 2].classList.add("img-left");
+    slidesArray[carouselLength - 2].classList.remove("img-hidden");
+
+    // for all other frames
+    slidesArray[carouselLength - 3].classList.remove("img-right");
+    slidesArray[carouselLength - 3].classList.add("img-hidden");
     
-        // moves slide into frame
-        slidesArray[carouselLength-1].classList.add("img-active");
-        slidesArray[carouselLength-1].classList.remove("img-left");
-
-        // lines up next frames
-        slidesArray[carouselLength-2].classList.add("img-left");
-        slidesArray[carouselLength-2].classList.remove("img-hidden");
-
-        // for all other frames
-        slidesArray[carouselLength-3].classList.remove("img-right");
-        slidesArray[carouselLength-3].classList.add("img-hidden");
-        // cycling through frames
-        slidesArray.unshift(slidesArray.pop())
+    // restarts array order
+    slidesArray.unshift(slidesArray.pop())
 
 }
 
-function playSlides() {
+function playSlides(event) {
     play = setInterval(slideRight, 2000);
     playButton.classList.add("play-hide");
     pauseButton.classList.remove("play-hide");
@@ -100,19 +102,21 @@ function pauseSlides() {
     pauseButton.classList.add("play-hide");
 }
 
-// event listener for clicking left and right
-rightArrow.addEventListener("click", slideRight);
-leftArrow.addEventListener("click", slideLeft);
-
-// event listener for left and right arrow keys
-document.addEventListener("keydown", function(event) {
+// function for pressing left and right arrow keys
+function keyPress(event) {
     if (event.which === 39) {
         return slideRight();
     }
     if (event.which === 37) {
         return slideLeft();
     }
-});
+}
+// event listener for clicking left and right
+rightArrow.addEventListener("click", slideRight);
+leftArrow.addEventListener("click", slideLeft);
+
+// event listener for left and right arrow keys
+document.addEventListener("keydown", keyPress);
 
 // event listener for clicking play and pause
 playButton.addEventListener("click", playSlides);
